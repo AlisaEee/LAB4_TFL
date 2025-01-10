220b18a9494d6e69ce23428bdd351167bcf2c71c
Egorova Alisa <e-alissa@mail.ru>
2025-01-10 15:25:10 +0300

    fix+part2

--- ast_reg.py
+++ ast_reg.py
@@ -12,6 +12,10 @@ class RegexParser:
         self.in_parse_lookahead = False
         self.save_group_ids = []
         self.save_ref_ids = []
+
+        self.rules = {}
+        self.ast_tree = {}
+        self.group_ids = {}
     def get_tokens(self):
         string_pattern = []
         while self.index != len(self.string):
@@ -20,9 +24,9 @@ class RegexParser:
                 letters = current
                 self.index+=1
                 #print(self.string[next_symbol])
-                while self.index < len(self.string) and 'a' <= self.string[self.index] <= 'z':
+                '''while self.index < len(self.string) and 'a' <= self.string[self.index] <= 'z':
                     letters+=self.string[self.index]
-                    self.index+=1
+                    self.index+=1'''
                 string_pattern.append(Token('letter', letters))
             else:
                 match current:
@@ -103,13 +107,14 @@ class RegexParser:
                 if self.count_cap_groups>9:
                     print("To many cap_groups")
                     return {}
+                id_group = self.count_cap_groups
                 self.save_group_ids.append(self.count_cap_groups)
                 node = self.alternation()
                 if self.index < len(self.pattern) and self.pattern[self.index].kind == 'right_bracket':
                     self.index+=1
                 else:
                     return {}
-                return {'kind':'capture_group','key':self.count_cap_groups,'value':node}
+                return {'kind':'capture_group','key':id_group,'value':node}
             if current.kind == 'non_capturing':
                 self.index+=1
                 node = self.alternation()
@@ -171,8 +176,88 @@ class RegexParser:
             if id not in self.save_group_ids:
                 print("Error: reference unknown")
                 return False
+        self.ast_tree = node
+        #print(node)
         return True
-       
+    def print_grammar(self,grammar_rules):
+        for nt in grammar_rules:
+            for rhs in grammar_rules[nt]:
+                print(nt,'->',rhs)
+        
+    def build_grammar(self,ast,rule_name="S",index = 0):
+        #ast = self.ast_tree
+        
+        #grammar_rules = {}
+        if ast['kind'] == 'capture_group':
+            # Для захватывающей группы создаем новое правило
+            
+            if ast['key'] not in self.group_ids.keys():
+                group_rule_name = f"G{index}"
+                self.group_ids[ast['key']] = group_rule_name
+                index+=1
+            else:
+                group_rule_name = self.group_ids[ast['key']]
+            
+            self.rules[rule_name]=[]
+            self.rules[rule_name].append(group_rule_name)
+            self.build_grammar(ast['value'], group_rule_name,index)
+        elif ast['kind'] == 'alternative':
+            # Обрабатываем альтернативы
+            alternatives = [] 
+            self.rules[rule_name] = []
+            for option in ast['value']:
+                name = rule_name + f'_{index}'
+                index += 1
+                self.rules[name] = []
+                alternatives.append(name)
+                self.build_grammar(option,name,index)
+            self.rules[rule_name].append(alternatives)
+        elif ast['kind'] == 'concatination':
+            # Обрабатываем конкатенацию
+            concatenation = []
+            for item in ast['value']:
+                name = rule_name + f'_{index}'
+                index += 1
+                self.rules[name] = []
+                concatenation.append(name)
+                self.build_grammar(item,name,index)
+            self.rules[rule_name] = []
+            self.rules[rule_name].append(concatenation)
+        elif ast['kind'] == 'letter':
+            # Обрабатываем литерал
+            self.rules[rule_name] = []
+            self.rules[rule_name].append(ast['value'])
+        elif ast['kind'] == 'referenseDigit':
+            # Обрабатываем ссылки на цифры
+            self.rules[rule_name] = []
+            referenseDigit = ast['value']
+            if referenseDigit not in self.group_ids:
+                self.rules[f"G{index}"] = []
+                self.group_ids[referenseDigit] = f"G{index}"
+            self.rules[rule_name].append(self.group_ids[referenseDigit])
+        elif ast['kind'] == 'repeat':
+            # Обрабатываем оператор *
+            self.rules[rule_name] = []
+            name = rule_name + f'_{index}'
+            index+=1
+            self.rules[rule_name].append([name,rule_name])
+            self.rules[rule_name].append('e')
+            self.build_grammar(ast['value'],name,index)
+        elif ast['kind'] == 'lookahead':
+            name = f'LOOK_{index}'
+            index+=1
+            self.rules[name] = []
+            self.rules[rule_name] = []
+            self.rules[rule_name].append(name)
+            self.rules[name].append('e')
+        elif ast['kind'] == 'non_capturing':  
+            self.rules[rule_name]=[]
+            capturing_rule_name = f"NonCap_{index}"
+            index+=1
+            self.rules[rule_name].append(capturing_rule_name)
+            self.build_grammar(ast['value'], capturing_rule_name,index)
+        #self.print_grammar(grammar_rules) lookahead non-capturing
+        return self.rules
 
 # Пример использования
 ok_patterns = [
@@ -208,14 +293,17 @@ error_patterns = [
     '()',
     
 ]
-for pattern in error_patterns:
+for pattern in ok_patterns:
     parser = RegexParser(pattern)
     
-    print(pattern)
+    print("Entered pattern: ",pattern)
     result = parser.get_tokens()
     if result:
         result = parser.parse()
         print(f"Pattern: {pattern} -> Valid: {result}")
+        if result:
+            grammar = parser.build_grammar(parser.ast_tree)
+            parser.print_grammar(grammar)
     else:
         print('Ошибка: token()')
         print(f"Pattern: {pattern} -> Valid: False")
